f4ad6a8377c02f4b6fe568486e73868ffef6bc08
wordisstuff <wordisstuff@gmail.com>
2024-12-30 10:37:54 -0600

    registration form

--- App.js
+++ App.js
@@ -1,22 +1,42 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import RegistrationScreen from "./Screens/RegistrationScreen";
+import { StatusBar } from 'expo-status-bar';
+import { ImageBackground, Modal, StyleSheet, Text, View } from 'react-native';
+import RegistrationScreen from './Screens/RegistrationScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>HELLO Wordisstuff!</Text>
-      <RegistrationScreen />
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <ImageBackground
+            source={require('./images/bg.png')}
+            style={styles.bg}
+            resizeMode="cover"
+        >
+            <View style={styles.container}>
+                <View style={styles.screen}>
+                    <RegistrationScreen style={styles.modal} />
+                </View>
+                <StatusBar style="auto" />
+            </View>
+        </ImageBackground>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+    },
+    screen: {
+        // position: 'absolute',
+        // top: 280,
+        // left: 'auto',
+        // width: 'auto',
+        // borderTopRightRadius: 25,
+        // borderTopLeftRadius: 25,
+    },
+    bg: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
--- Screens/RegistrationScreen.jsx
+++ Screens/RegistrationScreen.jsx
@@ -1,7 +1,101 @@
-import React from "react";
+import React, { useState } from 'react';
+import {
+    Alert,
+    Button,
+    Dimensions,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+} from 'react-native';
 
 const RegistrationScreen = () => {
-  return <div>RegistrationScreen</div>;
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const { width } = Dimensions.get('screen');
+
+    const onLogin = () => {
+        Alert.alert(`${name} = ${email}`);
+    };
+
+    return (
+        <View style={style.container}>
+            <Text style={style.title}>Реєстрація</Text>
+            <View style={style.inputForm}>
+                <TextInput
+                    value={name}
+                    onChangeText={setName}
+                    placeholder="Логін"
+                    style={style.input}
+                />
+                <TextInput
+                    value={email}
+                    onChangeText={setEmail}
+                    placeholder="Адреса електронної пошти"
+                    style={style.input}
+                />
+                <TextInput
+                    value={password}
+                    onChangeText={setPassword}
+                    placeholder="Пароль"
+                    style={style.input}
+                />
+                <TouchableOpacity onPress={onLogin} style={style.inputBtn}>
+                    <Text style={style.btnText}>Зареєстуватися</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={onLogin} style={style.loginLink}>
+                    <Text>Вже є акаунт? Увійти</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
 };
+
+const style = StyleSheet.create({
+    title: {
+        fontSize: 30,
+        marginBottom: 33,
+    },
+    inputForm: {
+        width: '100%',
+        gap: 16,
+        backgroundColor: '#fff',
+    },
+    input: {
+        fontSize: 16,
+        padding: 16,
+        borderWidth: 1,
+        borderColor: '#E8E8E8',
+        borderRadius: 8,
+        backgroundColor: '#F6F6F6',
+    },
+    container: {
+        backgroundColor: '#f0f',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputBtn: {
+        paddingVertical: 16,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 0,
+        borderStyle: 'solid',
+        borderRadius: 100,
+        backgroundColor: '#FF6C00',
+    },
+    btnText: {
+        color: '#ffffff',
+        fontSize: 16,
+        fontWeight: '400',
+    },
+    loginLink: {
+        backgroundColor: '#fff',
+        alignItems: 'center',
+    },
+});
 
 export default RegistrationScreen;
